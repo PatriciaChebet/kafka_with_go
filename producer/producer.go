95a7bbc07dd4b8988ce2d9f2f9b1b290bb4e0928
PatriciaChebet <patriciacchebet@gmail.com>
2022-04-04 19:05:37 +0300

    Updates kafka producer contacting kafka broker

---
+++ producer/producer.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	//Contacting the leader
+	conn, _ := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "topic-test", 0)
+	//set a write deadline
+	conn.SetWriteDeadline(time.Now().Add(time.Second * 10))
+
+	//write something
+	conn.WriteMessages(kafka.Message{Value: []byte("Hi Kafka again")})
+
+}
